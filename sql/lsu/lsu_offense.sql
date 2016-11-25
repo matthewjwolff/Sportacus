831d67dffa85c8f5f10da3ccf42da2f3ebfcebf9
garrettsiener <garrettsiener@gmail.com>
2016-11-25 16:32:33 -0600

    fixed g_id

--- sql/lsu/lsu_offense.sql
+++ sql/lsu/lsu_offense.sql
@@ -2,43 +2,43 @@
 
 -- vs Wisconsin
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (109,1,14,10,2,1,0,126,131,3,'23:03',3,2);
+VALUES (109,32,14,10,2,1,0,126,131,3,'23:03',3,2);
 
 -- vs Jacksonville St.
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (109,2,18,11,7,0,0,244,127,1,'26:51',2,5);
+VALUES (109,33,18,11,7,0,0,244,127,1,'26:51',2,5);
 
 -- vs Mississippi St.
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (106,3,22,16,7,1,0,177,215,0,'32:44',1,3);
+VALUES (106,34,22,16,7,1,0,177,215,0,'32:44',1,3);
 
 -- vs Auburn
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (103,4,14,13,4,0,0,220,118,2,'27:49',1,1);
+VALUES (103,25,14,13,4,0,0,220,118,2,'27:49',1,1);
 
 -- vs Missouri
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (108,5,30,11,6,1,0,418,216,1,'42:33',0,6);
+VALUES (108,35,30,11,6,1,0,418,216,1,'42:33',0,6);
 
 -- vs Southern Miss
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (109,6,17,9,5,0,0,183,276,1,'22:01',1,6);
+VALUES (109,36,17,9,5,0,0,183,276,1,'22:01',1,6);
 
 -- vs Ole Miss
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (105,7,20,11,4,0,0,311,204,2,'32:13',3,5);
+VALUES (105,37,20,11,4,0,0,311,204,2,'32:13',3,5);
 
 -- vs Alabama
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (101,8,6,15,4,0,2,33,92,5,'26:05',1,0);
+VALUES (101,9,6,15,4,0,2,33,92,5,'26:05',1,0);
 
 -- vs Arkansas
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (102,9,23,12,6,0,0,390,157,1,'35:11',2,5);
+VALUES (102,20,23,12,6,0,0,390,157,1,'35:11',2,5);
 
 -- vs Florida
 INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs)
-VALUES (108,10,23,14,7,3,1,219,204,1,'34:02',2,1);
+VALUES (108,38,23,14,7,3,1,219,204,1,'34:02',2,1);
 
 -- don't know if we're including this one but here it is anyway
 
