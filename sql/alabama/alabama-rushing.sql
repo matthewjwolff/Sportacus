49196bc98de6bda7d4fde860b6c89c98986c3bc2
Everett Moser <foolsboy137@gmail.com>
2016-11-25 16:16:14 -0600

    alabama rushing

---
+++ sql/alabama/alabama-rushing.sql
@@ -0,0 +1,94 @@
+-- rushing stats
+
+-- DAMIEN HARRIS
+
+-- vs USC
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (1, 15, 9, 138, 73, 0)
+
+-- vs WKU
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (2, 15, 11, 45, 11, 0)
+
+-- vs MISS
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (3, 15, 18, 146, 41, 0)
+
+-- vs KENT
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (4, 15, 4, 18, 9, 0)
+
+-- vs UK
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (5, 15, 2, 11, 7, 0)
+
+-- vs ARK
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (6, 15, 13, 122, 57, 0)
+
+-- vs TENN
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (7, 15, 13, 122, 57, 0)
+
+-- vs TA&M
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (8, 15, 17, 128, 30, 0)
+
+-- vs LSU
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (9, 15, 12, 53, 13, 0)
+
+-- vs MSST
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (10, 15, 3, 9, 4, 0)
+
+-- vs CHAT
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (11, 15, 13, 91, 25, 1)
+
+
+-- JOSHUA JACOBS
+
+-- vs USC
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (1, 16, 4, 20, 8, 0)
+
+-- vs WKU
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (2, 16, 0, 0, 0, 0)
+
+-- vs MISS
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (3, 16, 3, 33, 25, 0)
+
+-- vs KENT
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (4, 16, 11, 97, 24, 2)
+
+-- vs UK
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (5, 16, 16, 100, 28, 1)
+
+-- vs ARK
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (6, 16, 3, 57, 56, 0)
+
+-- vs TENN
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (7, 16, 3, 57, 56, 0)
+
+-- vs TA&M
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (8, 16, 11, 33, 8, 0)
+
+-- vs LSU
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (9, 16, 5, -4, 0, 0)
+
+-- vs MSST
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (10, 16, 9, 89, 43, 0)
+
+-- vs CHAT
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs) 
+VALUES (11, 16, 5, 17, 7, 0)
