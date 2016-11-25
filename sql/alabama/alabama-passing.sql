34bfb08ebd1ab2a1ad05ee730e9392949fb38058
Everett Moser <foolsboy137@gmail.com>
2016-11-25 16:36:38 -0600

    Cooper Bateman is a benchwarmer

---
+++ sql/alabama/alabama-passing.sql
@@ -0,0 +1,116 @@
+-- passing alabama
+
+-- HURTS
+
+-- vs USC
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (1, 13, 11, 6, 118, 1, 71, 2)
+
+-- vs WKU
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (2, 13, 36, 23, 287, 0, 52, 2)
+
+-- vs MISS
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (3, 13, 31, 19, 158, 0, 45, 0)
+
+-- vs KENT
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (4, 13, 24, 16, 164, 0, 34, 1)
+
+-- vs UK
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (5, 13, 33, 20, 262, 0, 46, 2)
+
+-- vs ARK
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (6, 13, 17, 13, 253, 1, 67, 2)
+
+-- vs TENN
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (7, 13, 27, 17, 172, 1, 36, 1)
+
+-- vs TA&M
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (8, 13, 25, 15, 164, 2, 45, 2)
+
+-- vs LSU
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (9, 13, 19, 10, 107, 1, 52, 0)
+
+-- vs MSST
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (10, 13, 37, 28, 347, 1, 67, 4)
+
+-- vs CHAT
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (11, 13, 21, 15, 136, 0, 47, 3)
+
+-- BATEMAN
+
+-- vs USC
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (1, 14, 1, 1, 5, 0, 5, 0)
+
+-- vs WKU
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (2, 14, 0, 0, 0, 0, 0, 0)
+
+-- vs MISS
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (3, 14, 0, 0, 0, 0, 0, 0)
+
+-- vs KENT
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (4, 14, 3, 2, -2, 0, 0, 0)
+
+-- vs UK
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (5, 14, 5, 5, 53, 0, 24, 0)
+
+-- vs ARK
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (6, 14, 0, 0, 0, 0, 0, 0)
+
+-- vs TENN
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (7, 14, 1, 1, 13, 0, 13, 0)
+
+-- vs TA&M
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (8, 14, 0, 0, 0, 0, 0, 0)
+
+-- vs LSU
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (9, 14, 0, 0, 0, 0, 0, 0)
+
+-- vs MSST
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (10, 14, 4, 3, 50, 0, 30, 1)
+
+-- vs CHAT
+
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs) 
+VALUES (11, 14, 0, 0, 0, 0, 0, 0)
+
