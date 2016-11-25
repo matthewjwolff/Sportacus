f6771fdc4a0821a174e76c3d6fb1868d4e3ef00f
Everett Moser <foolsboy137@gmail.com>
2016-11-25 17:16:16 -0600

    alabama returning is bruutal

---
+++ sql/alabama/alabama-returning.sql
@@ -0,0 +1,62 @@
+-- returning alabama
+
+-- vs USC
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 1, 4, 28, 24, 0)
+
+-- vs WKU
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 2, 5, 78, 24, 0)
+
+-- vs MISS
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 3, 5, 174, 85, 1)
+
+-- vs KENT
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 4, 4, 90, 75, 1)
+
+-- vs UK
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 5, 3, 65, 22, 0)
+
+-- vs ARK
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 6, 7, 102, 25, 0)
+
+
+-- vs TENN
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 7, 5, 136, 79, 1)
+
+
+-- vs TA&M
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 8, 6, 99, 32, 0)
+
+
+-- vs LSU
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 9, 3, 15, 14, 0)
+
+
+-- vs MSST
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 10, 1, 1, 1, 0)
+
+
+-- vs CHAT
+
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (101, 11, 5, 38, 14, 0)
+
