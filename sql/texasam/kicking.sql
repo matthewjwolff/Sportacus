c8af60368a8b95a7ee023f57a6a3cd557dd0178d
Matthew Wolff <mwolff3@lsu.edu>
2016-11-25 17:53:29 -0600

    defensive tables done

---
+++ sql/texasam/kicking.sql
@@ -0,0 +1,23 @@
+-- UCLA Gamecocks
+insert into kicking values (33, 53, 331, 7, 59, 0, 0, 0, 0, 0, 0);
+insert into kicking values (34, 53, 0, 0, 0, 23, 2, 1, 4, 4, 0);
+
+-- Prairie View Panthers
+insert into kicking values (33, 54, 75, 2, 48, 0, 0, 0, 0, 0, 0);
+insert into kicking values (34, 54, 0, 0, 0, 48, 3, 3, 8, 8, 0);
+
+-- South Carolina Gamecocks
+insert into kicking values (33, 55, 224, 5, 50, 0, 0 ,0, 0, 0, 0);
+insert into kicking values (34, 55, 0, 0, 0, 35, 2, 1, 3, 3, 0);
+
+-- Tennessee Volunteers
+insert into kicking values (33, 56, 419, 10, 55, 0, 0, 0, 0, 0, 0);
+insert into kicking values (34, 56, 0, 0, 0, 34, 2, 1, 6, 6, 0);
+
+-- New Mexico State Aggies
+insert into kicking values (33, 57, 83, 2, 50, 0, 0, 0, 0, 0, 0);
+insert into kicking values (34, 57, 0, 0, 0, 36, 2, 1, 7, 7, 0);
+
+-- Texas San Antonio Roadrunners
+insert into kicking values (33, 58, 181, 5, 44, 0, 0, 0, 0, 0, 0);
+insert into kicking values (34, 58, 0, 0, 0, 37, 3, 3, 2, 2, 0);
---
+++ sql/texasam/returning.sql
@@ -0,0 +1,17 @@
+-- UCLA Bruins
+insert into returning values (107, 53, 7, 153, 30, 0);
+
+-- Prairie View Panthers
+insert into returning values (107, 54, 3, 96, 73, 1);
+
+-- South Carolina Gamecocks
+insert into returning values (107, 55, 5, 99, 36, 0);
+
+-- Tennessee Volunteers
+insert into returning values (107, 56, 0, 0, 0, 0);
+
+-- New Mexico State Aggies
+insert into returning values (107, 57, 4, 220, 73, 2);
+
+-- Texas San Antonio Roadrunners
+insert into returning values (107, 58, )4, 51, 19, 0;
