f6acdaa5f5460eb36908d902e23c29df039feb68
Everett Moser <foolsboy137@gmail.com>
2016-11-25 17:31:19 -0600

    remembering semicolons so my compiler doesnt have to

--- sql/alabama/alabama-receiving.sql
+++ sql/alabama/alabama-receiving.sql
@@ -10,186 +10,186 @@
 -- vs USC
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (1, 17, 4, 113, 71, 2)
+VALUES (1, 17, 4, 113, 71, 2);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (1, 18, 2, 9, 10, 0)
+VALUES (1, 18, 2, 9, 10, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (1, 19, 0, 0, 0, 0)
+VALUES (1, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (1, 20, 1, 45, 45, 1)
+VALUES (1, 20, 1, 45, 45, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (1, 21, 0, 0, 0, 0)
+VALUES (1, 21, 0, 0, 0, 0);
 
 -- vs WKU
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (2, 17, 5, 90, 52, 1)
+VALUES (2, 17, 5, 90, 52, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (2, 18, 9, 129, 51, 1)
+VALUES (2, 18, 9, 129, 51, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (2, 19, 3, 13, 6, 0)
+VALUES (2, 19, 3, 13, 6, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (2, 20, 2, 56, 40, 0)
+VALUES (2, 20, 2, 56, 40, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (2, 21, 1, 14, 14, 0)
+VALUES (2, 21, 1, 14, 14, 0);
 
 -- vs MISS
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (3, 17, 4, 2, 5, 0)
+VALUES (3, 17, 4, 2, 5, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (3, 18, 8, 81, 45, 0)
+VALUES (3, 18, 8, 81, 45, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (3, 19, 0, 0, 0, 0)
+VALUES (3, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (3, 20, 2, 47, 30, 0)
+VALUES (3, 20, 2, 47, 30, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (3, 21, 0, 0, 0, 0)
+VALUES (3, 21, 0, 0, 0, 0);
 
 -- vs KENT
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (4, 17, 0, 0, 0, 0)
+VALUES (4, 17, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (4, 18, 1, 5, 5, 0)
+VALUES (4, 18, 1, 5, 5, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (4, 19, 0, 0, 0, 0)
+VALUES (4, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (4, 20, 1, 10, 10, 0)
+VALUES (4, 20, 1, 10, 10, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (4, 21, 3, 23, 15, 0)
+VALUES (4, 21, 3, 23, 15, 0);
 
 -- vs UK
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (5, 17, 0, 0, 0, 0)
+VALUES (5, 17, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (5, 18, 11, 174, 46, 2)
+VALUES (5, 18, 11, 174, 46, 2);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (5, 19, 2, 32, 17, 0)
+VALUES (5, 19, 2, 32, 17, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (5, 20, 1, 11, 11, 0)
+VALUES (5, 20, 1, 11, 11, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (5, 21, 2, 14, 8, 0)
+VALUES (5, 21, 2, 14, 8, 0);
 
 -- vs ARK
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (6, 17, 5, 120, 67, 0)
+VALUES (6, 17, 5, 120, 67, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (6, 18, 3, 14, 9, 1)
+VALUES (6, 18, 3, 14, 9, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (6, 19, 0, 0, 0, 0)
+VALUES (6, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (6, 20, 1, 6, 6, 0)
+VALUES (6, 20, 1, 6, 6, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (6, 21, 0, 0, 0, 0)
+VALUES (6, 21, 0, 0, 0, 0);
 
 -- vs TENN
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (7, 17, 8, 90, 36, 1)
+VALUES (7, 17, 8, 90, 36, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (7, 18, 6, 58, 31, 0)
+VALUES (7, 18, 6, 58, 31, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (7, 19, 0, 0, 0, 0)
+VALUES (7, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (7, 20, 0, 0, 0, 0)
+VALUES (7, 20, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (7, 21, 1, 13, 13, 0)
+VALUES (7, 21, 1, 13, 13, 0);
 
 -- vs TA&M
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (8, 17, 2, 57, 45, 0)
+VALUES (8, 17, 2, 57, 45, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (8, 18, 4, 27, 9, 1)
+VALUES (8, 18, 4, 27, 9, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (8, 19, 0, 0, 0, 0)
+VALUES (8, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (8, 20, 0, 0, 0, 0)
+VALUES (8, 20, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (8, 21, 0, 0, 0, 0)
+VALUES (8, 21, 0, 0, 0, 0);
 
 -- vs LSU
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (9, 17, 3, 55, 52, 0)
+VALUES (9, 17, 3, 55, 52, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (9, 18, 2, 23, 21, 0)
+VALUES (9, 18, 2, 23, 21, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (9, 19, 0, 0, 0, 0)
+VALUES (9, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (9, 20, 0, 0, 0, 0)
+VALUES (9, 20, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (9, 21, 1, 9, 9, 0)
+VALUES (9, 21, 1, 9, 9, 0);
 
 -- vs MSST
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (10, 17, 8, 156, 67, 3)
+VALUES (10, 17, 8, 156, 67, 3);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (10, 18, 4, 26, 20, 1)
+VALUES (10, 18, 4, 26, 20, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (10, 19, 0, 0, 0, 0)
+VALUES (10, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (10, 20, 1, 10, 10, 0)
+VALUES (10, 20, 1, 10, 10, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (10, 21, 1, 5, 5, 1)
+VALUES (10, 21, 1, 5, 5, 1);
 
 -- vs CHAT
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (11, 17, 0, 0, 0, 0)
+VALUES (11, 17, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (11, 18, 7, 94, 47, 1)
+VALUES (11, 18, 7, 94, 47, 1);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (11, 19, 0, 0, 0, 0)
+VALUES (11, 19, 0, 0, 0, 0);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (11, 20, 3, 1, 1, 2)
+VALUES (11, 20, 3, 1, 1, 2);
 
 INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs) 
-VALUES (11, 21, 0, 0, 0, 0)
+VALUES (11, 21, 0, 0, 0, 0);
