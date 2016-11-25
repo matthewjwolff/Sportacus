b53ca37b1a803ae30f4c96cbc9c3d4c72e0e529e
Everett Moser <foolsboy137@gmail.com>
2016-11-25 17:29:07 -0600

    awaiting clarification from judges...

---
+++ sql/alabama/alabama-kicking.sql
@@ -0,0 +1,57 @@
+-- kiking alabama ;)
+
+-- vs USC
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 1, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs WKU
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 2, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs MISS
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 3, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs KENT
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 4, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs UK
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 5, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs ARK
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 6, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs TENN
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 7, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs TA&M
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 8, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs LSU
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 9, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs MSST
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 10, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
+-- vs CHAT
+
+INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+VALUES (p_id, 11, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+
