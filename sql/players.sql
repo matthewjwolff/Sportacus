2179656b24e6529332ac01ed0526bebf44adc0f9
k-shelt <kalebshelton42@gmail.com>
2016-11-25 15:18:18 -0600

    Add files via upload

---
+++ sql/players.sql
@@ -0,0 +1,112 @@
+create table players(
+	p_id int,
+	f_name varchar(20),
+	l_name varchar(20),
+	t_id int,
+	position varchar(20),
+	number int,
+	class varchar(20)
+)
+
+--Alabama 
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("13","jalen",   "Hurts",      "101","QB",  "2", "FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("14","cooper",  "Bateman",    "101","QB",  "18","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("15","damien",  "Harris",     "101","RB",  "34","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("16","joshua",  "Jacobs",     "101","RB",  "25","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("17","ardarius","Stewart",    "101","WR",  "33","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("18","calvin",  "Ridley",     "101","WR",  "3", "SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("19","robert",  "Foster",     "101","WR",  "1", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("20","gehrig",  "Dieter",     "101","WR",  "11","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("21","trevon",  "Diggs",      "101","WR",  "7", "FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("22","adam",    "Griffith",   "101","K/PK","99","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("23","andy",    "Pappanastos","101","K/PK","92","JR");
+
+--Arkansas
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("1","austin",   "allen",    "102","QB",  "8", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("2","ty",       "storey",   "102","QB",  "5", "FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("3","keon",     "hatcher",  "102","WR",  "4", "SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("4","drew",     "morgan",   "102","WR",  "80","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("5","jeremy",   "sprinkle", "102","TE",  "83","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("6","dominique","reed",     "102","WR",  "3", "SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("7","jared",    "cornelius","102","WR",  "1", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("8","rawliegh", "williams", "102","RB",  "22","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("9","devwah",   "whaley",   "102","RB",  "21","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("10","toby",    "baker",    "102","K/PK","37","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("11","adam",    "mcfain",   "102","K/PK","48","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("12","cole",    "hedlund",  "102","K/PK","9", "SO");
+
+--Auburn
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("70","Sean",   "White",  "103","QB",  "13","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("71","Jeremy", "Johnson","103","QB",  "6", "SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("72","Kerryon","Johnson","103","RB",  "21","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("73","Kam",    "Martin", "103","RB",  "0", "FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("74","Tony",   "Stevens","103","WR",  "8", "SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("75","Ryan",   "Davis",  "103","WR",  "83","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("76","Eli",    "Stove",  "103","WR",  "12","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("77","Darius", "Slayton","103","WR",  "81","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("78","Kyle",   "Davis",  "103","WR",  "11","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("79","Daniel","Carlson", "103","K/PK","38","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("80","Kevin", "Phillips","103","K/PK","91","SR");
+
+--LSU
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("59","Danny",  "Etling",     "104","QB",  "16","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("60","Brandon","Harris",     "104","QB",  "6", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("61","Leonard","Fournette",  "104","RB",  "7", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("62","Derrius","Guice",      "104","RB",  "5", "SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("63","Travin", "Dural",      "104","WR",  "83","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("64","Malachi","Dupre",      "104","WR",  "15","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("65","D.J.",   "Chark",      "104","WR",  "82","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("66","Jazz",   "Ferguson",   "104","WR",  "86","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("67","Dee",    "Anderson",   "104","WR",  "11","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("68","Colby", "Delahoussaye","104","K/PK","42","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("69","Jack",  "Gonsoulin ",  "104","K/PK","35","FR");
+
+--Mississippi state
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("48","Nick",   "Fitzgerald","105","QB",  "7", "SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("49","Damian", "Williams",  "105","QB",  "11","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("50","Aeris",  "Williams",  "105","RB",  "27","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("51","Ashton", "Shumpert",  "105","RB",  "32","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("52","Fred",   "Ross",      "105","WR",  "8", "SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("53","Donald", "Gray",      "105","WR",  "6", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("54","Malik",  "Dear",      "105","WR",  "22","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("55","Keith",  "Mixon",     "105","WR",  "23","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("56","Jamal",  "Couch",     "105","WR",  "17","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("57","Westin", "Graves",    "105","K/PK","25","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("58","Logan",  "Cooke",     "105","K/PK","43","JR");
+
+
+--Ole Miss
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("34","Chad",     "Kelly",       "106","QB",  "10","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("36","Shea",     "Patterson",   "106","QB",  "20","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("37","Akeem",    "Judd",        "106","RB",  "21","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("38","Eugene",   "Brazley",     "106","RB",  "23","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("39","Evan",     "engram",      "106","TE",  "17","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("40","Damore'ea","Stringfellow","106","WR",  "3", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("41","Van",      "Jefferson",   "106","WR",  "12","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("42","Quincy",   "adeboyejo",   "106","WR",  "8", "SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("43","AJ",       "Brown",       "106","WR",  "1", "FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("44","Gary",     "Wunderlich",  "106","K/PK","97","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("45","Will",     "Gleason",     "106","K/PK","94","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("46","Zedrick",  "Woods",       "106","DB",  "36","SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("47","Carlos",   "Davis",       "106","DB",  "23","SR");
+
+--Texas A&M
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("24","Jake",     "Hubenak",    "107","QB",  "10","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("25","Nick",     "Starkel",    "107","QB",  "17","FR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("26","Speedy",   "Noil",       "107","WR",  "2", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("27","Ricky",    "Seals-Jones","107","WR",  "9", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("28","Josh",     "Reynolds",   "107","TE",  "11","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("29","Christian","Kirk",       "107","WR",  "3", "SO");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("30","Jeremy",   "Tabuyo",     "107","WR",  "19","SR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("31","James",    "White",      "107","RB",  "20","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("32","Keith",    "Ford",       "107","RB",  "7", "JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("33","Shane",    "Tripucka",   "107","K/PK","46","JR");
+insert into players ("p_id","f_name","l_name","t_id","position","number","class") values("34","Daniel",   "LaCamera",   "107","K/PK","36","SO");
+
+
+
+
+
+
+
+
