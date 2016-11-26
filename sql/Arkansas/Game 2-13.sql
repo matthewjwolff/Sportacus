8bd85ffa5efe4b158b6c2d1c057b3adb65aacd97
ajhlsu <aljherbert@gmail.com>
2016-11-26 14:42:44 -0600

    Update Game 2-13.sql

--- sql/Arkansas/Game 2-13.sql
+++ sql/Arkansas/Game 2-13.sql
@@ -6,7 +6,7 @@ insert into kicking values(12, 13, 0, 0, 0, 38, 3, 2, 3, 3, 0);
 insert into kicking values(10, 13, 304, 6, 59, 0, 0, 0, 0, 0, 0);
 
 
-insert into defense values(102, 13, 3, 1, 1, 1, 0, 0, 572, 5, 26:53);
+insert into defense values(102, 13, 3, 1, 1, 1, 0, 0, 572, 5, '26:53');
 
 
 insert into rushing values(13, 8, 28, 137, 37, 0);
@@ -28,4 +28,4 @@ insert into receiving values(13, 6, 1, 11, 11, 0);
 insert into receiving values(13, 7, 2, 17, 17, 0);
 
 
-insert into offense values(102, 13, 20, 16, 7, 0, 0, 180, 223, 1, 33:07, 1, 4);
+insert into offense values(102, 13, 20, 16, 7, 0, 0, 180, 223, 1, '33:07', 1, 4);
