7ec9dc4661647f466147a752085034978138c202
garrettsiener <garrettsiener@gmail.com>
2016-11-26 12:05:05 -0600

    lsu rushing sql

---
+++ sql/lsu/lsu_rushing.sql
@@ -0,0 +1,61 @@
+-- LSU rushing
+
+-- vs Wisconsin
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (32,61,23,138,30,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (32,62,2,3,3,0);
+
+-- vs Jacksonville
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (33,61,2,1,1,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (33,62,19,155,44,1);
+
+-- vs Miss St.
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (34,61,28,147,25,2);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (34,62,5,19,7,0);
+
+-- vs Auburn
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (25,61,16,101,35,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (25,62,3,62,52,0);
+
+-- vs Missouri
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (35,61,3,12,5,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (35,62,17,163,42,3);
+
+-- vs Southern Miss
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (36,61,0,0,0,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (36,62,16,162,61,2);
+
+-- vs Ole Miss
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (37,61,16,284,78,3);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (37,62,16,57,13,1);
+
+-- vs Alabama
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (9,61,17,35,9,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (9,62,2,8,5,0);
+
+-- vs Arkansas
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (20,61,17,98,17,3);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (20,62,21,252,96,2);
+
+-- vs Florida
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (38,61,12,40,11,0);
+INSERT INTO rushing (g_id, p_id, attempts, yards, longest_rush, TDs)
+VALUES (38,62,19,83,20,1);
