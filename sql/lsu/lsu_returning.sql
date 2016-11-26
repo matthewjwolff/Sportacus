d2c3a5fb923427d83bbbaa80cacc2b7648a48481
garrettsiener <garrettsiener@gmail.com>
2016-11-26 11:37:39 -0600

    adding lsu return sql

---
+++ sql/lsu/lsu_returning.sql
@@ -0,0 +1,41 @@
+-- LSU returning
+
+-- vs Wisconsin
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,32,3,25,17,0);
+
+-- vs Jacksonville
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,33,7,123,60,1);
+
+-- vs Miss St.
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,34,6,96,29,0);
+
+-- vs Auburn
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,25,0,0,0,0);
+
+-- vs Missouri
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,35,2,4,4,0);
+
+-- vs Southern Miss
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,36,5,79,30,0);
+
+-- vs Ole Miss
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,37,8,112,25,0);
+
+-- vs Alabama
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,9,4,43,27,0);
+
+-- vs Arkansas
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,20,4,3,12,0);
+
+-- vs Florida
+INSERT INTO Returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
+VALUES (104,38,1,12,12,0);
