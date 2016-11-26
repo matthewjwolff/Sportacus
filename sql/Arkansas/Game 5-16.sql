191c8e74aead6d5ea4d8f0220d361cde66879467
ajhlsu <aljherbert@gmail.com>
2016-11-26 14:45:59 -0600

    Update Game 5-16.sql

--- sql/Arkansas/Game 5-16.sql
+++ sql/Arkansas/Game 5-16.sql
@@ -4,7 +4,7 @@ insert into returning values(102, 16, 3, 62, 33, 0);
 insert into kicking values(12, 16, 0, 0, 0, 44, 2, 1, 7, 7, 0);
 
 
-insert into defense values(102, 16, 2, 2, 1, 2, 0, 0, 313, 1, 31:55);
+insert into defense values(102, 16, 2, 2, 1, 2, 0, 0, 313, 1, '31:55');
 
 
 insert into rushing values(16, 8, 13, 126, 29, 0);
@@ -26,4 +26,4 @@ insert into receiving values(16, 6, 0, 0, 0, 0);
 insert into receiving values(16, 7, 4, 106, 35, 2);
 
 
-insert into offense values(102, 16, 25, 7, 2, 2, 0, 353, 206, 0, 28:05, 2, 6);
+insert into offense values(102, 16, 25, 7, 2, 2, 0, 353, 206, 0, '28:05', 2, 6);
