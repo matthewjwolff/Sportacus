ec76060ad78a1c430f992e42a659efe09dea738c
Matthew Wolff <mwolff3@lsu.edu>
2016-11-26 00:07:54 -0600

    SQL Compliance and automation

--- sql/populate_postgres.sh
+++ sql/populate_postgres.sh
@@ -8,9 +8,11 @@ dropdb sportacus
 createdb sportacus
 
 # Step 2: root level scripts
-psql -f offenseteams.sql sportacus
-psql -f defensegames.sql sportacus
+psql -f teams.sql sportacus
+psql -f games.sql sportacus
 psql -f players.sql sportacus
+psql -f offense.sql sportacus
+psql -f defense.sql sportacus
 
 # step 3, team scripts
 psql -f */*.sql sportacus
---
+++ sql/teams.sql
@@ -0,0 +1,16 @@
+create table teams (
+	t_id int,
+	name varchar(255),
+	location varchar(255),
+	Primary Key(t_id)
+);
+
+insert into teams("t_id","name","location") values('101', 'Alabama', 'Tuscaloosa, Alabama');
+insert into teams("t_id","name","location") values('102', 'Arkansas', 'Fayetteville, Arkansas');
+insert into teams("t_id","name","location") values('103', 'Auburn', 'Auburn, Alabama');
+insert into teams("t_id","name","location") values('104', 'LSU', 'Baton Rouge, Louisiana');
+insert into teams("t_id","name","location") values('105', 'Ole Miss', 'Oxford, Mississippi');
+insert into teams("t_id","name","location") values('106', 'Mississippi State', 'Starkville, Mississippi');
+insert into teams("t_id","name","location") values('107', 'Texas A&M', 'College Station, Texas');
+insert into teams("t_id","name","location") values('108', 'Generic SEC', 'SEC, USA');
+insert into teams("t_id","name","location") values('109', 'Generic Out Of Conference', 'Party in the, USA');
