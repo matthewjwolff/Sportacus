e959f576e830f090aff3f0abe21de5847a753011
garrettsiener <garrettsiener@gmail.com>
2016-11-26 10:35:41 -0600

    adding passing sql

---
+++ sql/lsu/lsu_passing.sql
@@ -0,0 +1,41 @@
+-- LSU Passing
+
+-- vs Wisconsin
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (32,59,21,12,131,2,31,1);
+
+-- vs Jacksonville
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (33,59,14,6,100,1,46,1);
+
+-- vs Miss St.
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (34,59,30,19,215,0,37,1);
+
+-- vs Auburn
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (25,59,27,15,118,0,32,1);
+
+-- vs Missouri
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (35,59,30,19,216,0,41,0);
+
+-- vs Southern Miss
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (36,59,18,11,276,1,80,3);
+
+-- vs Ole Miss
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (37,59,28,19,204,1,40,1);
+
+-- vs Alabama
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (9,59,24,11,92,1,9,0);
+
+-- vs Arkansas
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (20,59,16,10,17,0,48,0);
+
+-- vs Florida
+INSERT INTO passing (g_id, p_id, attempts, completions, yards, interceptions, longest_pass, TDs)
+VALUES (38,59,25,14,204,0,33,0);
