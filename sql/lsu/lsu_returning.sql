afd7666910940632dceb60ebd8720171fa2b5fc5
garrettsiener <garrettsiener@gmail.com>
2016-11-26 14:43:55 -0600

    column names + preturn

--- sql/lsu/lsu_returning.sql
+++ sql/lsu/lsu_returning.sql
@@ -1,41 +1,31 @@
 -- LSU preturns
 
 -- vs Wisconsin
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,32,3,25,17,0);
+INSERT INTO preturning VALUES (104,32,3,25,17,0);
 
 -- vs Jacksonville
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,33,7,123,60,1);
+INSERT INTO preturning VALUES (104,33,7,123,60,1);
 
 -- vs Miss St.
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,34,6,96,29,0);
+INSERT INTO preturning VALUES (104,34,6,96,29,0);
 
 -- vs Auburn
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,25,0,0,0,0);
+INSERT INTO preturning VALUES (104,25,0,0,0,0);
 
 -- vs Missouri
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,35,2,4,4,0);
+INSERT INTO preturning VALUES (104,35,2,4,4,0);
 
 -- vs Southern Miss
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,36,5,79,30,0);
+INSERT INTO preturning VALUES (104,36,5,79,30,0);
 
 -- vs Ole Miss
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,37,8,112,25,0);
+INSERT INTO preturning VALUES (104,37,8,112,25,0);
 
 -- vs Alabama
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,9,4,43,27,0);
+INSERT INTO preturning VALUES (104,9,4,43,27,0);
 
 -- vs Arkansas
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,20,4,3,12,0);
+INSERT INTO preturning VALUES (104,20,4,3,12,0);
 
 -- vs Florida
-INSERT INTO returning (t_id, g_id, total_returns, total_return_yards, longest_return, touchdowns)
-VALUES (104,38,1,12,12,0);
+INSERT INTO preturning VALUES (104,38,1,12,12,0);
