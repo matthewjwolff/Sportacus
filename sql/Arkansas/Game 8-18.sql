3f376f72a16b82a74da9900f8c33981cd79119d8
ajhlsu <aljherbert@gmail.com>
2016-11-26 14:05:02 -0600

    Update Game 8-18.sql

--- sql/Arkansas/Game 8-18.sql
+++ sql/Arkansas/Game 8-18.sql
@@ -1,12 +1,12 @@
-insert into Returning ("t_id","g_id","total_returns","total_return_yards","longest_return","touchdowns") values(102, 18, 2, 29, 16, 0);
+insert into returning ("t_id","g_id","total_returns","total_return_yards","longest_return","touchdowns") values(102, 18, 2, 29, 16, 0);
 
 
-insert into Kicking ("p_id","g_id","total_punting_yards","total_punts","longest_punt","longest_field_goal","FG_attempted","PAT_made","total_touchbacks") values(11, 18, 0, 0, 0, 54, 1, 1, 0, 0, 0);
+insert into kicking ("p_id","g_id","total_punting_yards","total_punts","longest_punt","longest_field_goal","FG_attempted","PAT_made","total_touchbacks") values(11, 18, 0, 0, 0, 54, 1, 1, 0, 0, 0);
 
-insert into Kicking ("p_id","g_id","total_punting_yards","total_punts","longest_punt","longest_field_goal","FG_attempted","PAT_made","total_touchbacks") values(10, 18, 415, 10, 58, 0, 0, 0, 0, 0, 0);
+insert into kicking ("p_id","g_id","total_punting_yards","total_punts","longest_punt","longest_field_goal","FG_attempted","PAT_made","total_touchbacks") values(10, 18, 415, 10, 58, 0, 0, 0, 0, 0, 0);
 
 
-insert into Defense ("t_id","g_id","sacks","interceptions","pick_6s","fumbles_forced","fumbles_into_tds","safeties","yards_allowed","tds_allowed","time_on_field") values(102, 18, 0, 0, 0, 0, 0, 0, 632, 8, 27:23);
+insert into defense ("t_id","g_id","sacks","interceptions","pick_6s","fumbles_forced","fumbles_into_tds","safeties","yards_allowed","tds_allowed","time_on_field") values(102, 18, 0, 0, 0, 0, 0, 0, 632, 8, 27:23);
 
 
 insert into rushing ("g_id","p_id","attempts","yards","longest_rush","TDs") values(18, 8, 13, 22, 8, 0);
