942d11f0bb8d9ad88ebefe1c448007b18013ad5f
disneyprincesschristo <christopherbroussard74@gmail.com>
2016-11-26 14:56:20 -0600

    Update kicking.sql

--- sql/auburn/kicking.sql
+++ sql/auburn/kicking.sql
@@ -4,89 +4,89 @@
 
 -- vs CU
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 22, 222, 5, 68, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 22, NULL, NULL, NULL, 53, 2, 2, 1, 1, NULL);
 
 -- vs ASU
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 23, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 23, NULL, NULL, NULL, 42, 3, 3, 6, 6, NULL);
 
 -- vs TAMU
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 24, 380, 7, 52, NULL, NULL, NULL, NULL, NULL, 1);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 24, NULL, NULL, NULL, 37, 1, 1, 1, 1, NULL);
 
 -- vs LSU
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 25, 120, 3, 46, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 25, NULL, NULL, NULL, 51, 6, 6, 0, 0, NULL);
 
 -- vs ULM
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 26, 0, 0, 0, 0, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 26, NULL, NULL, NULL, 0, 1, 0, 8, 8, NULL);
 
 -- vs MS
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 27, 147, 4, 41, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 27, NULL, NULL, NULL, 25, 1, 1, 5, 5, NULL);
 
 -- vs AR
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 18, 119, 3, 53, NULL, NULL, NULL, NULL, NULL, 1);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 18, NULL, NULL, NULL, 0, 0, 0, 8, 8, NULL);
 
 -- vs OM
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 28, 86, 2, 50, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 28, NULL, NULL, NULL, 47, 4, 4, 4, 4, NULL);
 
 -- vs VANDY
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 29, 93, 2, 47, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 29, NULL, NULL, NULL, 53, 4, 3, 2, 2, NULL);
 
 
 -- vs UGA
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 30, 353, 8, 50, NULL, NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 30, NULL, NULL, NULL, 0, 0, 0, 1, 1, NULL);
 
 -- vs UAA
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (80, 31, 79, 2, 43, NULL, NULL, NULL, NULL, NULL, 1);
 
-INSERT INTO Kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
+INSERT INTO kicking (p_id, g_id, total_punting_yards, total_punts, longest_punt, longest_field_goal, FG_attempted, FG_made, PAT_attempted, PAT_made, total_touchbacks)
 VALUES (79, 31, NULL, NULL, NULL, 41, 2, 3, 7, 7, NULL);
