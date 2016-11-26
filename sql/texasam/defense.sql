9227f3c33d414d4f8a6d8ee303905fd21bd83176
disneyprincesschristo <christopherbroussard74@gmail.com>
2016-11-26 14:40:37 -0600

    Update defense.sql

--- sql/texasam/defense.sql
+++ sql/texasam/defense.sql
@@ -1,17 +1,17 @@
 -- UCLA Bruins
-insert into Defense values (107, 53, 5, 3, 0, 2, 0, 0, 468, 2, '33:18');
+insert into defense values (107, 53, 5, 3, 0, 2, 0, 0, 468, 2, '33:18');
 
 -- Prairie View Panthers
-insert into Defense values (107, 54, 2, 1, 0, 2, 0, 1, 205, 0, '31:42');
+insert into defense values (107, 54, 2, 1, 0, 2, 0, 1, 205, 0, '31:42');
 
 -- South Carolina Gamecocks
-insert into Defense values (107, 55, 3, 1, 0, 3, 0, 0, 378, 1, '31:47');
+insert into defense values (107, 55, 3, 1, 0, 3, 0, 0, 378, 1, '31:47');
 
 -- Tennessee Volunteers
-insert into Defense values (107, 56, 4, 2, 0, 6, 0, 0, 684, 5, '32:23');
+insert into defense values (107, 56, 4, 2, 0, 6, 0, 0, 684, 5, '32:23');
 
 -- New Mexico State Aggies
-insert into Defense values (107, 57, 3, 0, 0, 1, 0, 0, 316, 1, '29:48');
+insert into defense values (107, 57, 3, 0, 0, 1, 0, 0, 316, 1, '29:48');
 
 -- Texas San Antonio Roadrunners
-insert into Defense values (107, 58, 6, 0, 0, 2, 0, 0, 252, 1, '31:14');
+insert into defense values (107, 58, 6, 0, 0, 2, 0, 0, 252, 1, '31:14');
