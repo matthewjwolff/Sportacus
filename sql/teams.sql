b6b402dde46a52ffac79b98c9a0e78771b1a627a
Alan Fos <aef___@hotmail.com>
2016-11-26 12:36:07 -0600

    Update teams.sql

--- sql/teams.sql
+++ sql/teams.sql
@@ -5,12 +5,12 @@ create table teams (
 	Primary Key(t_id)
 );
 
-insert into teams(t_id,name, location) values(101, 'Alabama', 'Tuscaloosa, Alabama');
-insert into teams(t_id,name, location) values(102, 'Arkansas', 'Fayetteville, Arkansas');
-insert into teams(t_id,name, location) values(103, 'Auburn', 'Auburn, Alabama');
-insert into teams(t_id,name, location) values(104, 'LSU', 'Baton Rouge, Louisiana');
-insert into teams(t_id,name, location) values(105, 'Ole Miss', 'Oxford, Mississippi');
-insert into teams(t_id,name, location) values(106, 'Mississippi State', 'Starkville, Mississippi');
-insert into teams(t_id,name, location) values(107, 'Texas A&M', 'College Station, Texas');
-insert into teams(t_id,name, location) values(108, 'Generic SEC', 'SEC, USA');
-insert into teams(t_id,name, location) values(109, 'Generic Out Of Conference', 'Party in the, USA');
+insert into teams(t_id, name, location) values(101, 'Alabama', 'Tuscaloosa, Alabama');
+insert into teams(t_id, name, location) values(102, 'Arkansas', 'Fayetteville, Arkansas');
+insert into teams(t_id, name, location) values(103, 'Auburn', 'Auburn, Alabama');
+insert into teams(t_id, name, location) values(104, 'LSU', 'Baton Rouge, Louisiana');
+insert into teams(t_id, name, location) values(105, 'Ole Miss', 'Oxford, Mississippi');
+insert into teams(t_id, name, location) values(106, 'Mississippi State', 'Starkville, Mississippi');
+insert into teams(t_id, name, location) values(107, 'Texas A&M', 'College Station, Texas');
+insert into teams(t_id, name, location) values(108, 'Generic SEC', 'SEC, USA');
+insert into teams(t_id, name, location) values(109, 'Generic Out Of Conference', 'Party in the, USA');
