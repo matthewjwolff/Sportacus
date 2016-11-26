cdca637111450574d85b5a697dbca4cbad14ea9f
ajhlsu <aljherbert@gmail.com>
2016-11-26 14:42:01 -0600

    Update Game 10-20.sql

--- sql/Arkansas/Game 10-20.sql
+++ sql/Arkansas/Game 10-20.sql
@@ -6,7 +6,7 @@ insert into kicking values(11, 20, 0, 0, 0, 24, 1, 1, 1, 1, 0);
 insert into kicking values(10, 20, 302, 7, 51, 0, 0, 0, 0, 0, 0);
 
 
-insert into defense values(102, 20, 1, 0, 0, 1, 0, 0, 547, 5, 35:11);
+insert into defense values(102, 20, 1, 0, 0, 1, 0, 0, 547, 5, '35:11');
 
 
 insert into rushing values(20, 8, 13, 49, 18, 0);
@@ -28,4 +28,4 @@ insert into receiving values(20, 6, 2, 58, 44, 1);
 insert into receiving  values(20, 7, 3, 15, 6, 0);
 
 
-insert into offense values(102, 20, 13, 13, 5, 0, 0, 81, 210, 3, 24:49, 2, 1);
+insert into offense values(102, 20, 13, 13, 5, 0, 0, 81, 210, 3, '24:49', 2, 1);
