0e5a43646304a233977590080efc2cf09f2190ab
disneyprincesschristo <christopherbroussard74@gmail.com>
2016-11-26 14:54:59 -0600

    Update defense.sql

--- sql/auburn/defense.sql
+++ sql/auburn/defense.sql
@@ -1,45 +1,45 @@
 -- defense stats
 
 -- vs CU
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 22, 0, 1, 0, 1, 0, 0, 399, 2, '30:29');
 
 -- vs ASU
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 23, 4, 0, 0, 0, 0, 0, 326, 2, '24:18');
 
 -- vs TAMU
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 24, 1, 0, 0, 2, 0, 0, 478, 2, '28:56');
 
 -- vs LSU
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 25, 3, 0, 0, 1, 0, 0, 338, 1, '27:49');
 
 -- vs ULM
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 26, 2, 1, 0, 2, 0, 1, 251, 1, '26:45');
 
 -- vs MS
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 27, 3, 1, 0, 1, 0, 0, 289, 2, '26:38');
 
 -- vs AR
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 18, 4, 1, 0, 1, 0, 0, 215, 0, '32:37');
 
 -- vs OM
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 28, 2, 1, 0, 0, 0, 0, 570, 3, '27:33');
 
 -- vs VANDY
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 29, 1, 1, 0, 2, 0, 0, 394, 1, '35:09');
 
 -- vs UGA
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 30, 2, 1, 0, 1, 0, 0, 343, 0, '39:09');
 
 -- vs AAM
-INSERT INTO Defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
+INSERT INTO defense (t_id, g_id, sacks, interceptions, pick_6s, fumbles_forced, fumbles_into_tds, safeties, yards_allowed, tds_allowed, time_on_field)
 VALUES (103, 31, 2, 2, 0, 0, 0, 0, 130, 0, '27:29');
