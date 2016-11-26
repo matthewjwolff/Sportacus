cd7600fce0322124725e36e0782e3a691dcaf905
ajhlsu <aljherbert@gmail.com>
2016-11-26 14:47:30 -0600

    Update Game 9-19.sql

--- sql/Arkansas/Game 9-19.sql
+++ sql/Arkansas/Game 9-19.sql
@@ -6,7 +6,7 @@ insert into kicking values(11, 19, 0, 0, 0, 36, 1, 1, 4, 4, 0);
 insert into kicking values(10, 19, 241, 6, 51, 0, 0, 0, 0, 0, 0);
 
 
-insert into defense values(102, 19, 3, 2, 0, 1, 0, 0, 241, 0, 20:39);
+insert into defense values(102, 19, 3, 2, 0, 1, 0, 0, 241, 0, '20:39');
 
 
 insert into rushing values(19, 8, 26, 148, 41, 2);
@@ -28,4 +28,4 @@ insert into receiving values(19, 6, 0, 0, 0, 0);
 insert into receiving values(19, 7, 0, 0, 0, 0);
 
 
-insert into offense values(102, 19, 22, 14, 6, 0, 0, 223, 243, 1, 39:21, 1, 3);
+insert into offense values(102, 19, 22, 14, 6, 0, 0, 223, 243, 1, '39:21', 1, 3);
