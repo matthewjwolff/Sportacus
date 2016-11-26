d99005c79a13e6745e682cb6a7693632d7085bb4
garrettsiener <garrettsiener@gmail.com>
2016-11-26 11:09:32 -0600

    receiving.sql

---
+++ sql/lsu/lsu_receiving.sql
@@ -0,0 +1,119 @@
+-- LSU Receiving
+
+-- vs Wisconsin
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (32,63,5,50,15,1);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (32,64,3,24,12,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (32,65,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (32,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (32,67,0,0,0,0);
+
+-- vs Jacksonville
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (33,63,1,5,5,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (33,64,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (33,65,1,12,12,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (33,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (33,67,0,0,0,0);
+
+-- vs Miss St.
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (34,63,4,40,16,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (34,64,4,54,17,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (34,65,3,52,37,1);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (34,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (34,67,0,0,0,0);
+
+-- vs Auburn
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (25,63,4,47,16,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (25,64,4,21,10,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (25,65,3,11,8,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (25,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (25,67,0,0,0,0);
+
+-- vs Missouri
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (53,63,4,34,12,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (35,64,4,37,12,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (35,66,2,17,10,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (35,67,0,0,0,0);
+
+-- vs Southern Miss
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (36,63,2,22,13,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (36,64,3,100,63,2);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (36,65,3,91,80,1);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (36,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (36,67,0,0,0,0);
+
+-- vs Ole Miss
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (37,63,4,48,21,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (37,64,5,52,18,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (37,65,3,45,40,1);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (37,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (37,67,0,0,0,0);
+
+-- vs Alabama
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (9,63,1,3,3,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (9,64,3,17,11,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (9,65,3,45,41,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (9,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (9,67,0,0,0,0);
+
+-- vs Arkansas
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (20,63,3,31,15,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (20,64,2,60,48,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (20,65,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (20,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (20,67,0,0,0,0);
+
+-- vs Florida
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (38,63,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (38,64,2,39,33,0):
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (38,65,2,46,30,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (38,66,0,0,0,0);
+INSERT INTO receiving (g_id, p_id, receptions, yards, longest_reception, TDs)
+VALUES (38,67,2,14,7,0);
