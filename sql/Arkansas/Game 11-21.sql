1dd10d1009416e02053f72660c82dfe79bd2bbda
ajhlsu <aljherbert@gmail.com>
2016-11-26 14:15:37 -0600

    Update Game 11-21.sql

--- sql/Arkansas/Game 11-21.sql
+++ sql/Arkansas/Game 11-21.sql
@@ -1,29 +1,29 @@
-insert into returning ("t_id","g_id","total_returns","total_return_yards","longest_return","touchdowns") values(102, 21, 5, 91, 28, 0);
+insert into returning values(102, 21, 5, 91, 28, 0);
 
 
-insert into kicking ("p_id","g_id","total_punting_yards","total_punts","longest_punt","longest_field_goal","FG_attempted","PAT_made","total_touchbacks") values(11, 21, 0, 0, 0, 34, 2, 1, 6, 5, 0);
+insert into kicking values(11, 21, 0, 0, 0, 34, 2, 1, 6, 5, 0);
 
 
-insert into defense ("t_id","g_id","sacks","interceptions","pick_6s","fumbles_forced","fumbles_into_tds","safeties","yards_allowed","tds_allowed","time_on_field") values(102, 21, 0, 0, 0, 0, 0, 0, 533, 6, 26:47);
+insert into defense values(102, 21, 0, 0, 0, 0, 0, 0, 533, 6, 26:47);
 
 
-insert into rushing ("g_id","p_id","attempts","yards","longest_rush","TDs") values(21, 8,16, 209, 72, 4);
+insert into rushing values(21, 8,16, 209, 72, 4);
 
-insert into rushing ("g_id","p_id","attempts","yards","longest_rush","TDs") values(21, 9, 19, 122, 50, 1);
-
-
-insert into passing ("g_id","p_id","attempts","completions","yards","interceptions","longest_pass","TDs") values(21, 1, 25, 18, 303, 0, 34, 2);
+insert into rushing values(21, 9, 19, 122, 50, 1);
 
 
-insert into receiving ("g_id","p_id","receptions","yards","longest_reception","TDs") values(21, 3, 4, 65, 33, 2);
-
-insert into receiving ("g_id","p_id","receptions","yards","longest_reception","TDs") values(21, 4, 6, 91, 34, 0);
-
-insert into receiving ("g_id","p_id","receptions","yards","longest_reception","TDs") values(21, 5, 2, 37, 24, 0);
-
-insert into receiving ("g_id","p_id","receptions","yards","longest_reception","TDs") values(21, 6, 0, 0, 0, 0);
-
-insert into receiving ("g_id","p_id","receptions","yards","longest_reception","TDs") values(21, 7, 0, 0, 0, 0);
+insert into passing values(21, 1, 25, 18, 303, 0, 34, 2);
 
 
-insert into offense ("t_id","g_id","first_downs","3rd_down_att","3rd_down_conv","4th_down_att","4th_down_conv","total_rushing","total_passing","sacks_allowed","time_of_possession","turnovers","TDs") values(102, 21, 27, 11, 7, 2, 2, 357, 304, 0, 33:13, 0, 8);
+insert into receiving values(21, 3, 4, 65, 33, 2);
+
+insert into receiving values(21, 4, 6, 91, 34, 0);
+
+insert into receiving values(21, 5, 2, 37, 24, 0);
+
+insert into receiving values(21, 6, 0, 0, 0, 0);
+
+insert into receiving values(21, 7, 0, 0, 0, 0);
+
+
+insert into offense values(102, 21, 27, 11, 7, 2, 2, 357, 304, 0, 33:13, 0, 8);
