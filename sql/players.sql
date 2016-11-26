019a1df0b53c04e778ace91513368fd7640eb470
Matthew Wolff <mwolff3@lsu.edu>
2016-11-26 13:34:25 -0600

    fixed missed quotes

--- sql/players.sql
+++ sql/players.sql
@@ -11,8 +11,8 @@ create table players(
 --Alabama
 insert into players values(13,'jalen',   'Hurts',      101,'QB',  2, 'FR');
 insert into players values(14,'cooper',  'Bateman',    101,'QB',  18,'JR');
-insert into players values(15,'damien',  'Harris',     101,'RB",  34,'SO');
-insert into players values(16,'joshua',  'Jacobs',     101,'RB",  25,'FR');
+insert into players values(15,'damien',  'Harris',     101,'RB',  34,'SO');
+insert into players values(16,'joshua',  'Jacobs',     101,'RB',  25,'FR');
 insert into players values(17,'ardarius','Stewart',    101,'WR',  33,'JR');
 insert into players values(18,'calvin',  'Ridley',     101,'WR',  3, 'SO');
 insert into players values(19,'robert',  'Foster',     101,'WR',  1, 'JR');
@@ -50,7 +50,7 @@ insert into players values(80,'Kevin',  'Phillips',103,'K/PK',91,'SR');
 
 --LSU
 insert into players values(59,'Danny',  'Etling',     104,'QB',  16,'JR');
-insert into players values(60,'Brandon','Harris',     104,'QB',  6, 'JR");
+insert into players values(60,'Brandon','Harris',     104,'QB',  6, 'JR');
 insert into players values(61,'Leonard','Fournette',  104,'RB',  7, 'JR');
 insert into players values(62,'Derrius','Guice',      104,'RB',  5, 'SO');
 insert into players values(63,'Travin', 'Dural',      104,'WR',  83,'SR');
@@ -64,7 +64,7 @@ insert into players values(69,'Josh',  'Growden',     104,'K/PK',38,'FR');
 --Ole Miss
 insert into players values(34,'Chad',     'Kelly',       106,'QB',  10,'SR');
 insert into players values(36,'Shea',     'Patterson',   106,'QB',  20,'FR');
-insert into players values(37,'Akeem',    'Judd",        106,'RB',  21,'SR');
+insert into players values(37,'Akeem',    'Judd',        106,'RB',  21,'SR');
 insert into players values(38,'Eugene',   'Brazley',     106,'RB',  23,'JR');
 insert into players values(39,'Evan',     'engram',      106,'TE',  17,'SR');
 insert into players values(40,'Damore\'ea','Stringfellow',106,'WR', 3, 'JR');
@@ -75,7 +75,7 @@ insert into players values(44,'Gary',     'Wunderlich',  106,'K/PK',97,'JR');
 insert into players values(45,'Will',     'Gleason',     106,'K/PK',94,'JR');
 insert into players values(46,'Zedrick',  'Woods',       106,'DB',  36,'SO');
 insert into players values(47,'Carlos',   'Davis',       106,'DB',  23,'SR');
-			   
+
 --Mississippi state
 insert into players values(48,'Nick',   'Fitzgerald',105,'QB',  7, 'SO');
 insert into players values(49,'Damian', 'Williams',  105,'QB',  11,'JR');
