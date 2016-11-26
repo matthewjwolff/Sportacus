cb305e84e8f6f32b09a0a0bce93b550fdd7d04f8
Everett Moser <foolsboy137@gmail.com>
2016-11-26 14:29:16 -0600

    good 2 go

--- sql/alabama/alabama-offense.sql
+++ sql/alabama/alabama-offense.sql
@@ -1,50 +1,50 @@
 -- offense stats
 
 -- vs USC
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 1, 15, 15, 6, 1, 1, 242, 223, 3, '30:43', 2, 7);
 
 -- vs WKU
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 2, 23, 18, 9, 2, 2, 124, 351, 3, '35:44', 1, 5);
 
 -- vs MISS
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 3, 23, 15, 7, 0, 0, 334, 158, 2, '35:23', 1, 6);
 
 -- vs KENT
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 4, 26, 14, 6, 3, 2, 285, 217, 2, '33:33', 0, 6);
 
 -- vs UK
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 5, 25, 14, 9, 1, 0, 173, 315, 3, '29:47', 1, 4);
 
 -- vs ARK
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 6, 16, 8, 4, 0, 0, 264, 253, 2, '22:53', 3, 7);
 
 -- vs TENN
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 7, 28, 12, 6, 1, 0, 409, 185, 2, '31:33', 2, 7);
 
 -- vs TA&M
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 8, 28, 14, 7, 1, 0, 287, 164, 2, '35:50', 2, 4);
 
 -- vs LSU
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 9, 16, 17, 7, 2, 1, 216, 107, 1, '33:55', 2, 1);
 
 -- vs MSST
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 10, 26, 13, 7, 1, 0, 218, 397, 1, '27:22', 2, 6);
 
 -- vs CHAT
-INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 VALUES (101, 11, 14, 13, 4, 1, 1, 196, 136, 3, '25:54', 2, 4);
 
 -- vs AUB
---INSERT INTO offense (t_id, g_id, first_downs, 3rd_down_att, 3rd_down_conv, 4th_down_att, 4th_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
+--INSERT INTO offense (t_id, g_id, first_downs, third_down_att, third_down_conv, fourth_down_att, fourth_down_conv, total_rushing, total_passing, sacks_allowed, time_of_possession, turnovers, TDs) 
 --VALUES (101, 12, 26, 13, 7, 1, 0, 218, 397, 1, '27:22', 2, 6);
 
